ebd2430eb917059289bdd24ff72d912a43a3f1c6
mujtabasaqib <mujtabasaqib99@gmail.com>
2025-07-10 18:25:34 +0500

    test: added basic unit tests for core functionality

---
+++ api/src/tests/app.test.ts
@@ -0,0 +1,103 @@
+import { describe, it, expect } from 'vitest'
+
+describe('LangLantern App', () => {
+  describe('Authentication', () => {
+    it('should allow a new user to register', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to login', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to switch roles as a contributor or a learner', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to logout', () => {
+      expect(1).toBe(2)
+    })
+  })
+
+  describe('Contributions section', () => {
+    it('should allow a contributer to add flashcards', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a contributer to add grammar notes', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a contributer to add audio notes', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a contributer to add tags', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a contributer to categorize his contributions based on difficulty', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a contributer to make collections of related content', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a contributer to make a course', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a contributer to add independent tests not tied to resources or courses', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow contributed content to be upvoted or downvoted by users', () => {
+      expect(1).toBe(2)
+    })
+  })
+
+  describe('Learning section', () => {
+    it('should allow a user to subscribe to a course / learning path', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to take lessons for each course', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to take assessments for each course', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to take tests related to specific resources', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to track his progress', () => {
+      expect(1).toBe(2)
+    })
+  })
+
+   describe('Forum section', () => {
+    it('should allow a user to share experiences with others in a forum like space', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a user to post replies', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow a thread to be associated with tags and categories', () => {
+      expect(1).toBe(2)
+    })
+
+    it('should allow role based access to show relevant threads', () => {
+      expect(1).toBe(2)
+    })
+
+     it('should allow a user to filter content', () => {
+      expect(1).toBe(2)
+    })
+  })
+});
