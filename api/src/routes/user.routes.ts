fbd60bf360da8c16b2bad13a8b0ea7594c8f2190
anjelica <144828225+anjelica0224@users.noreply.github.com>
2025-07-16 15:35:00 +0530

    feat: api endpoints for forum posts

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { me } from "../controller/user.controller";
-import { post, postUpdate, allPosts, postbyId, postsbyAuthorId, deletePostsbyId} from "../controller/post.controller";
+import { deletePostsbyId, getDiscussionPosts, getMainPosts, post, postUpdate, postbySlug} from "../controller/post.controller";
 
 const router = Router();
 
 router.get("/me", me);
 router.post("/posts", post)
-router.get("/posts", allPosts)
-router.get("/posts/:id", postbyId)
-router.get("/posts/author/:author", postsbyAuthorId)
+router.post("/posts/:slug", postbySlug)
+router.get("/posts", getMainPosts)
+router.get("/posts/:slug", getDiscussionPosts)
 router.patch("/posts/:id", postUpdate)
-router.patch("/posts/:id/delete", deletePostsbyId);
+router.delete("/posts/:id/delete", deletePostsbyId);
 
 export default router;
--- api/src/services/post.services.ts
+++ api/src/services/post.services.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
 import prisma from "./prisma";
+import { v4 as uuidv4 } from 'uuid';
 
 interface PostFormat{
   title:string,
@@ -14,9 +14,6 @@ export async function createPost(data: PostFormat) {
     const userExists = await prisma.user.findUnique({
       where: {
         id: authorId
-      },
-      select: {
-        id: true
       }
     })
 
@@ -27,17 +24,19 @@ export async function createPost(data: PostFormat) {
         title,
         body,
         authorId,
-        slug: `${title.split(" ").join("-").toLowerCase()}-${Date.now().toString(36)}`,
+        slug: `${title.split(" ").join("-").toLowerCase()}-${uuidv4()}`,
         createdAt: new Date(),
+        isMainPost: true,
       },
-      select: {
-        id: true,
-        title: true,
-        body: true,
-        slug: true,
-        author: true,
-        createdAt: true,
-        authorId: true,
+      include:{
+        author: {
+          select:{
+            username: true,
+            id: true,
+            name: true,
+            email: true,
+          }
+        }
       }
     });
     
@@ -65,56 +64,78 @@ export async function updatePost(updatedData: PostFormat, postid:string){
   }
 }
 
-export async function getAllPosts() {
-  try {
-    const posts = await prisma.post.findMany({
-      where:{
-        isDeleted: false
+export async function createPostbySlug(slug: string, data: PostFormat){
+  try{
+    const {body, authorId} = data;
+    const title = await prisma.post.findFirst({
+      where: {slug: slug, isMainPost: true},
+      select: {title: true}
+    })
+    const post = await prisma.post.create({
+      data:{
+        title: title?.title || "none",
+        body,
+        authorId,
+        slug: slug,
+        createdAt: new Date(),
+        isMainPost: false,
       },
-      include: {
-        author: true,
+      include:{
+        author: {
+          select:{
+            username: true,
+            id: true,
+            name: true,
+            email: true,
+          }
+        }
       }
-    });
-    
-    return posts;
-  } catch (error) {
-    console.error('Error fetching all posts:', error);
-    throw error;
-  }
-}
-
-export async function getPostById(postId: string) {
-  try {
-    const post = await prisma.post.findFirst({
-      where: { id: postId, isDeleted: false },
-      include: {
-        author: true,
-      }
-    });
-    
-    if (!post) {
-      throw new Error("Post not found");
-    }
-    
+    })
     return post;
-  } catch (error) {
-    console.error('Error fetching post by ID:', error);
+  }
+  catch(error){
+    console.error('Error creating post by slug:', error);
     throw error;
   }
 }
 
-export async function getPostsByAuthorId(authorId: string) {
-  try {
+export async function getAllMainPosts(){
+  try{
     const posts = await prisma.post.findMany({
-      where: { authorId: authorId, isDeleted: false},
+      where: {isMainPost: true},
       include: {
         author: true,
+        comments: true,
       }
-    });
-    
+    })
     return posts;
-  } catch (error) {
-    console.error('Error fetching posts by author ID:', error);
+  }
+  catch(error){
+    console.error('Error fetching all main posts:', error);
+    throw error;
+  }
+}
+
+export async function getRelatedPosts(slug: string){
+  try{
+    const posts = await prisma.post.findMany({
+      where: {slug: slug},
+      include: {
+        author: {
+          select:{
+            username: true,
+            id: true,
+            name: true,
+            email: true,
+          }
+        },
+        comments: true,
+      }
+    })
+    return posts;
+  }
+  catch(error){
+    console.error('Error fetching related posts:', error);
     throw error;
   }
 }
@@ -130,7 +151,12 @@ export const deletePosts = async (postId: string)=>{
 
     const deletePost = await prisma.post.update({
       where: {id: postId},
-      data: {isDeleted: true}
+      data: {
+        status: "DELETED"
+      },
+      select: {
+        status: true,
+      }
     })
 
     return deletePost;
