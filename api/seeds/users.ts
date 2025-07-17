5f2ddfff780e5e0c21146a8413d642a80a00982c
anjelica <144828225+anjelica0224@users.noreply.github.com>
2025-07-17 14:07:24 +0530

    refactor: seed data for forum

---
+++ api/seeds/users.ts
@@ -0,0 +1,72 @@
+export const demoUsers = [
+  {
+    id: '66b1a1234567890123456789',
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    username: 'johndoe',
+    password: 'hashedpassword123'
+  },
+  {
+    id: '66b1a1234567890123456790',
+    name: 'Jane Doe',
+    email: 'janedoe@example.com',
+    username: 'janedoe',
+    password: 'hashedpassword456'
+  },
+  {
+    id: '66b1a1234567890123456791',
+    name: 'Jake Smith',
+    email: 'jake@example.com',
+    username: 'jake',
+    password: 'hashedpassword789'
+  },
+  {
+    id: '66b1a1234567890123456792',
+    name: 'Jane Smith',
+    email: 'jane@example.com',
+    username: 'jane',
+    password: 'hashedpassword012'
+  },
+  {
+    id: '66b1a1234567890123456793',
+    name: 'John Smith',
+    email: 'john@example.com',
+    username: 'john',
+    password: 'hashedpassword345'
+  },
+  {
+    id: '66b1a1234567890123456794',
+    name: 'Anonymous Duck',
+    email: 'duck@example.com',
+    username: 'anonymous_duck',
+    password: 'hashedpassword678'
+  },
+  {
+    id: '66b1a1234567890123456795',
+    name: 'Anonymous Cat',
+    email: 'cat@example.com',
+    username: 'anonymous_cat',
+    password: 'hashedpassword901'
+  },
+  {
+    id: '66b1a1234567890123456796',
+    name: 'Anonymous Horse',
+    email: 'horse@example.com',
+    username: 'anonymous_horse',
+    password: 'hashedpassword234'
+  },
+  {
+    id: '66b1a1234567890123456797',
+    name: 'Anonymous Dog',
+    email: 'dog@example.com',
+    username: 'anonymous_dog',
+    password: 'hashedpassword567'
+  },
+  {
+    id: '66b1a1234567890123456798',
+    name: 'Anonymous Monkey',
+    email: 'monkey@example.com',
+    username: 'anonymous_monkey',
+    password: 'hashedpassword890'
+  }
+];
